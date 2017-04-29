e8c70113138f9359b6b05cfc3c90a035cb4bbd6c
ckcks12 <wyoducc@gmail.com>
2017-04-29 11:52:12 +0900

    main -> signup

--- app/src/main/java/hackathonbark/com/cookie/MainActivity.java
+++ app/src/main/java/hackathonbark/com/cookie/MainActivity.java
@@ -17,7 +17,7 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private CallbackManager callbackManager;
 
     @Override
@@ -25,36 +25,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        com.wang.avi.AVLoadingIndicatorView avi = (com.wang.avi.AVLoadingIndicatorView)findViewById(R.id.avi);
-        avi.show();
+//        com.wang.avi.AVLoadingIndicatorView avi = (com.wang.avi.AVLoadingIndicatorView)findViewById(R.id.avi);
+//        avi.show();
 
 
         // Facebook Login
-        callbackManager = CallbackManager.Factory.create();
-        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
-                Log.d("eunchan", "success");
-            }
-
-            @Override
-            public void onCancel() {
-                Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_LONG).show();
-                Log.d("eunchan", "cancel");
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
-                Log.d("eunchan", "error");
-            }
-        });
-
+//        callbackManager = CallbackManager.Factory.create();
+//        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+//                Log.d("eunchan", "success");
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//                Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_LONG).show();
+//                Log.d("eunchan", "cancel");
+//            }
+//
+//            @Override
+//            public void onError(FacebookException error) {
+//                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+//                Log.d("eunchan", "error");
+//            }
+//        });
+        findViewById(R.id.btnSignUp).setOnClickListener(this);
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btnSignUp:
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+        }
     }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        callbackManager.onActivityResult(requestCode, resultCode, data);
+//    }
 }
