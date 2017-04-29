9ccfdf26793dd6c687553b0510f381cb1317a56a
ckcks12 <wyoducc@gmail.com>
2017-04-29 16:14:09 +0900

    *

--- app/src/main/java/hackathonbark/com/cookie/MainActivity.java
+++ app/src/main/java/hackathonbark/com/cookie/MainActivity.java
@@ -3,9 +3,18 @@ package hackathonbark.com.cookie;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private CallbackManager callbackManager;
@@ -19,30 +28,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        avi.show();
 
 
-        // Facebook Login
-//        callbackManager = CallbackManager.Factory.create();
-//        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-//            @Override
-//            public void onSuccess(LoginResult loginResult) {
-//                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
-//                Log.d("eunchan", "success");
-//            }
-//
-//            @Override
-//            public void onCancel() {
-//                Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_LONG).show();
-//                Log.d("eunchan", "cancel");
-//            }
-//
-//            @Override
-//            public void onError(FacebookException error) {
+//         Facebook Login
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(getApplicationContext(), "반갑습니다", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(getApplicationContext(), TodayActivity.class));
+                Log.d("eunchan", "success");
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "취소되었습니다", Toast.LENGTH_LONG).show();
+                Log.d("eunchan", "cancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
 //                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
-//                Log.d("eunchan", "error");
-//            }
-//        });
+                Log.d("eunchan", "error");
+            }
+        });
 
         findViewById(R.id.btnLogin).setOnClickListener(this);
         findViewById(R.id.btnSignUp).setOnClickListener(this);
+        findViewById(R.id.btnFacebook).setOnClickListener(this);
     }
 
     @Override
@@ -56,14 +68,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnSignUp:
                 startActivity(new Intent(this, SignUpActivity.class));
                 break;
+
+            case R.id.btnFacebook:
+                LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "user_friends"));
+                break;
         }
 
 
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        callbackManager.onActivityResult(requestCode, resultCode, data);
-//    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
--- app/src/main/java/hackathonbark/com/cookie/TodayCookieActivity.java
+++ app/src/main/java/hackathonbark/com/cookie/TodayCookieActivity.java
@@ -1,9 +1,26 @@
 package hackathonbark.com.cookie;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.UUID;
 
 /**
  * Created by BaeSungSin on 2017-04-29.
@@ -16,15 +33,65 @@ public class TodayCookieActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_cookie);
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                URL url = null;
+                try {
+                    url = new URL("https://i0jhkm18rh.execute-api.ap-northeast-2.amazonaws.com/startup/cookie?user_id=1");
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    InputStream in = new BufferedInputStream(con.getInputStream());
+                    String res = convertStreamToString(in);
+                    Log.d("eunchan", res);
 
-  //      findViewById(R.id.btnCookie).setOnClickListener(this);
+                    JSONObject json = new JSONObject(res);
+                    final String str = json.getJSONObject("result").getString("memo");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView textView = (TextView) findViewById(R.id.textViewCookie);
+//                            Toast.makeText(TodayCookieActivity.this, str, Toast.LENGTH_SHORT).show();
+
+                            Typeface type  = Typeface.createFromAsset(getAssets(), "DXPnMStd-Regular.otf");
+                            textView.setTypeface(type);
+                            textView.setText(str);
+
+                        }
+                    });
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     public void onClick(View v) {
         switch(v.getId()) {
-            case R.id.btnCookie:
-                startActivity(new Intent(this, SignUpActivity.class));
-                break;
         }
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 }
